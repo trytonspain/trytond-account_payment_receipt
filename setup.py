584a0817bdd4f69bc92feb53b973e52cc025580a
V?ctor Ramos <victor@calidae.com>
2017-09-28 14:04:45 +0000

    setup.py fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 import ConfigParser
 
 MODULE = 'account_payment_receipt'
-PREFIX = 'nantic'
+PREFIX = 'trytonspain'
 MODULE2PREFIX = {
     'jasper_reports': 'trytonspain',
 }
@@ -45,7 +45,7 @@ setup(
     version=info.get('version', '0.0.1'),
     description='',
     long_description=read('README'),
-    author='NaN·tic',
+    author='NaNï¿½tic',
     url='http://www.nan-tic.com/',
     download_url="https://bitbucket.org/nantic/trytond-%s" % MODULE,
     package_dir={'trytond.modules.%s' % MODULE: '.'},
